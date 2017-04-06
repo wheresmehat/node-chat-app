6e23986e9061cc5d18d75614aa174e9f2e36fbcd
wheresmehat <csstogo@gmail.com>
2017-04-06 11:45:40 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,20 @@ io.on("connection", (socket) => {
 
     console.log("New user connected");
 
+    socket.emit("newMessage", {
+
+        from: "Admin",
+        text: "Welcome to the chat app",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit("newMessage", {
+
+        from: "Admin",
+        text: "New user joined",
+        createdAt: new Date().getTime()
+    });
+
     socket.on("createMessage", (message) => {
 
         console.log("createMessage", message);
@@ -26,6 +40,14 @@ io.on("connection", (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        /*socket.broadcast.emit("newMessage", {
+
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });*/
+
     });
 
     socket.on("disconnect", () => {
